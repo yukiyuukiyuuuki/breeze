19d1818798682f3804170d843217c567bc7e0b78
yu <naritayu@cy-com.co.jp>
2025-06-09 16:31:00 +0900

    init commit

---
+++ src/test/java/com/example/breeze/BreezeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.breeze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreezeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
