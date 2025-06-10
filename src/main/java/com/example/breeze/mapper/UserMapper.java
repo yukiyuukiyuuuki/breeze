2e7b606f997ca5cfd3f7b574cc1a89bc2b0baef7
KosakaAyano <ayakosaka@cy-com.co.jp>
2025-06-10 15:13:03 +0900

    UserMapper add Delete

--- src/main/java/com/example/breeze/mapper/UserMapper.java
+++ src/main/java/com/example/breeze/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.example.breeze.mapper;
 
 import com.example.breeze.entity.User;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -9,9 +10,12 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
   @Select("SELECT user_id, name, password, subscribe_date FROM user WHERE user_id = #{id}")
-  User selectUserById(String id);
+  User sellectUserById(String id);
 
   @Insert("INSERT INTO user (name, password, subscribe_date) VALUES (#{name}, #{password}, #{subscribe_date})")
-  void insertUser(UserForm userForm);
+  void insertUser(User user);
+
+  @Delete("DELETE FROM user")
+  void deleteUser(long userId);
 
 }
