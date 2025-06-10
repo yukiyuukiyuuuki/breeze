9ee4980080de061b07f2f1fd1e5a427fcf7a4f29
yahagitomo <yahagitomo@cy-com.co.jp>
2025-06-10 10:21:55 +0900

    delete file "musicform"

--- src/main/java/com/example/breeze/form/MusicForm.java
+++
@@ -1,9 +0,0 @@
-package com.example.music_management.form;
- import lombok.Data;
- import java.time.LocalTime;
- @Data
- public class MusicForm {
-    private String title;
-    private LocalTime duration;
-    private long albumId;
- }
