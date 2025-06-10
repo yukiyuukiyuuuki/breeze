7d2df9c24132ae658265f9a900738b64a7e93cf1
yahagitomo <yahagitomo@cy-com.co.jp>
2025-06-10 14:01:59 +0900

    re edit

--- src/main/java/com/example/breeze/repository/WhisperRepository.java
+++ src/main/java/com/example/breeze/repository/WhisperRepository.java
@@ -16,7 +16,7 @@ public class WhisperRepository {
   }
     */
   public List<Whisper> selectAllWhispers() { // 引数などのチェック必要
-    return whisperMapper.selectAllWhisper();
+    return whisperMapper.selectAllWhisper(); // sellect -> select で動作するように揃えた。
   }
   public void insertWhisper(Whisper whisper) {
     whisperMapper.insertWhisper(whisper);
