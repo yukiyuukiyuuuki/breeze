401ddf88f6a5ccefb5b6e0df11b3261a34bc9820
yu <naritayu@cy-com.co.jp>
2025-06-16 12:16:36 +0900

    remove imports that do not need

--- src/main/java/com/example/breeze/controller/UserCtrl.java
+++ src/main/java/com/example/breeze/controller/UserCtrl.java
@@ -3,18 +3,12 @@ package com.example.breeze.controller;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.breeze.dataformat.form.UserForm;
-import com.example.breeze.dataformat.form.WhisperForm;
 import com.example.breeze.security.CustomUserDetails;
 import com.example.breeze.service.UserService;
-import com.example.breeze.service.WhisperService;
 
-import ch.qos.logback.core.model.Model;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserCtrl {
