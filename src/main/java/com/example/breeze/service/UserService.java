8f25ea9ad5b8b85bfa1342fbc27a90978f5ad9c7
yu <naritayu@cy-com.co.jp>
2025-06-18 11:06:28 +0900

    changed policy that other user can not change whiseper

--- src/main/java/com/example/breeze/service/UserService.java
+++ src/main/java/com/example/breeze/service/UserService.java
@@ -3,6 +3,10 @@ package com.example.breeze.service;
 import com.example.breeze.dataformat.entity.User;
 import com.example.breeze.dataformat.form.UserForm;
 import com.example.breeze.db.repository.UserRepository;
+import com.example.breeze.security.CustomUserDetails;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +28,7 @@ public class UserService {
     UserRepository.insertUser(user);
   }
 
-  public User getUserById(long UserId){
+  public User getUserById(long UserId) {
     return UserRepository.selectUserById(UserId);
   }
 
@@ -34,5 +38,15 @@ public class UserService {
 
   public boolean checkUserAlreadyExist(String name) {
     return UserRepository.checkUserAlreadyExist(name);
+  }
+
+  public long getCurrentUserId() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    long userId = -1;
+    if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
+      CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+      userId = customUserDetails.getUserId();
+    }
+    return userId;
   }
 }
